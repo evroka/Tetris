9f69881bd148e655ff09dccbde4004d6f30d9461
evroka <nata_safonova@inbox.ru>
2019-08-11 15:55:17 +0400

    Creating a shape view class

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-import Game from './src/game.js'
+import Game from './src/game.js';
+import View from './src/view.js';
+
+const root = document.querySelector('#root');
 
 const game = new Game();
+const view = new View(root, 320, 640, 20, 10);
 
 window.game = game;
+window.view = view;
 
-console.log(game);
+document.addEventListener('keydown', event => {
+	switch (event.key) {
+		case 'ArrowLeft':
+			game.movePieceLeft();
+			view.render(game.getState());
+			break;
+		case 'ArrowUp': 
+			game.rotatePiece();
+			view.render(game.getState());
+			break;
+		case 'ArrowRight':
+			game.movePieceRight();
+			view.render(game.getState());
+			break;
+		case 'ArrowDown':
+			game.movePieceDown();
+			view.render(game.getState());
+			break;
+	}
+});
